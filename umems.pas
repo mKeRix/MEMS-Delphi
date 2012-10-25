65bc6ac1a8ce8e45b67fd1fe76f0b6a143cd6904
Heiko Rothe <mkerix@outlook.com>
2012-10-25 19:34:21 +0200

    Initial commit
    Simple project for Lazarus.

---
+++ umems.pas
@@ -0,0 +1,26 @@
+unit uMEMS;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TMEMS = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  MEMS: TMEMS;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
